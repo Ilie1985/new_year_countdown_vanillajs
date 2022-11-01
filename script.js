360160e338e2ed65ab25b2170d2b8e8e59cb7bb9
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-01 17:23:25 +0000

    set background year

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+
+// Set background year
+year.innerText = currentYear + 1;
+
+
+
+
+
+
 const updateCountdown = () => {
   const currentTime = newDate();
   const diff = newYearTime - currentTime;
