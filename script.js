22b882daffae39759e21f4a4f0f4167deffecb20
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-01 17:21:28 +0000

    updateCountdown func

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const countdown = document.getElementById('countdown');
-const year = document.getElementById('year');
-const loading = document.getElementById('loading');
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+const updateCoutdown = () => {
+  const currentTime = newDate();
+  const diff = newYearTime - currentTime;
+};
+
+updateCoutdown();
